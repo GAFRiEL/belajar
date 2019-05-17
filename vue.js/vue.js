d9cd0a4133120e8057ccaf237360fac3bb8eba99
GAFRiEL <gafrieljunior@gmail.com>
2019-05-17 22:36:11 +0800

    add vue 1st project

--- vue.js/vue.js
+++ vue.js/vue.js
@@ -1,31 +1,107 @@
-var app = new Vue({
-    el: '#app',
-    data:{
-        product: "Socks",
-        image: "../archive/sock-black.jpg",
-        inventory: true,
-        details: ["80% cotton", "20% polyester", "gender neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "black",
-                variantImage: "../archive/sock-black.jpg"
-            },
-            {
-                variantId: 2235,
-                variantColor: "grey",
-                variantImage: "../archive/sock-grey.jpg"
-            }
-        ],
-        cart: 0
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="container row">
+        <div class="col-md-3">
+            <img :src="image" class="w-100">
+        </div>
+
+        <div>
+            <h3>{{ title }}</h3>
+
+            <p v-if="inventory">In Stock</p>
+            <p v-else="">Out Of Stock</p>
+            <p>Shipping : $ {{ shipping }}</p>
+
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                class="color-box"   
+                :style="{ backgroundColor: variant.variantColor }"
+                @mouseover="updateProduct(index)"
+            >
+            </div><br>
+
+            <div class="row">
+                <button 
+                    v-on:click="addToCart"
+                    :disabled="!inventory"
+                    class="btn btn-secondary">
+                    Add to Cart
+                </button>
+            </div>
+
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            brand: "School",
+            product: "Socks",
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "gender neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "black",
+                    variantImage: "../archive/sock-black.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "grey",
+                    variantImage: "../archive/sock-grey.jpg",
+                    variantQuantity: 0
+                }
+            ]
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inventory() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return "2.99"
+        }
+    }
 
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
 })
